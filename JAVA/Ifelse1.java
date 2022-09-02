6ad3ae45d1f97cb695c89fa296337aeef06195f8
nagarajan512k <112387736+nagarajan512k@users.noreply.github.com>
2022-09-02 20:46:18 +0530

    Add files via upload

---
+++ JAVA/Ifelse1.java
@@ -0,0 +1,23 @@
+package com.ust;
+
+class Ifelse1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=10;
+		
+		//checks if number is greater than 0
+		if(number>0) {
+			System.out.println("The number is positive:");
+		}
+		
+		//execute this block
+		//if number is not greater than 0
+		else {
+			System.out.println("This number is not positive:");
+			
+		}
+		System.out.println("Statement outside if..else block");
+	}
+
+}
