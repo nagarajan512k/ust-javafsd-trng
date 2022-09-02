945c6686da912ce817f36e4734d156dac80cf050
nagarajan512k <112387736+nagarajan512k@users.noreply.github.com>
2022-09-02 20:30:34 +0530

    Add files via upload

---
+++ JAVA/Arrayusingforeach.java
@@ -0,0 +1,18 @@
+package com.ust;
+
+public class Arrayusingforeach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] vowels= {'a','e','i','o','u'};
+		
+		
+		//iterating through an array using the for-each loop
+		for(char item:vowels) {
+			System.out.println(item);
+		}
+		
+		}
+	}
+
+
