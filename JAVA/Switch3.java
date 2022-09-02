bc2da40c4d4fac7b54caf1d1b3ed4d04b6d3cace
nagarajan512k <112387736+nagarajan512k@users.noreply.github.com>
2022-09-02 21:02:57 +0530

    Add files via upload

---
+++ JAVA/Switch3.java
@@ -0,0 +1,27 @@
+package com.ust;
+
+class Switch3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int expression=2;
+		
+		//switch statement to check size
+		switch(expression) {
+		case 1:
+			System.out.println("Case1");
+			
+			//matching case
+		case 2:
+			System.out.println("Case2");
+			
+		case 3:
+			System.out.println("Case3");
+			
+			default:
+				System.out.println("Default case");
+		}
+
+	}
+
+}
