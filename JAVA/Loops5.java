30189497d86384480fd3103f0d4d770aaf780495
nagarajan512k <112387736+nagarajan512k@users.noreply.github.com>
2022-09-02 20:55:32 +0530

    Add files via upload

---
+++ JAVA/Loops5.java
@@ -0,0 +1,16 @@
+package com.ust;
+import java.util.Scanner;
+public class Loops5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1,n=5;
+		
+		//do..while loop from 1 to 5
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<=n);
+	}
+
+}
