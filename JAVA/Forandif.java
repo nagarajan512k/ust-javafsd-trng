ab18953e3031f49b30d598bc81a4b7a839edf838
nagarajan512k <112387736+nagarajan512k@users.noreply.github.com>
2022-09-02 20:43:02 +0530

    Add files via upload

---
+++ JAVA/Forandif.java
@@ -0,0 +1,21 @@
+package com.ust;
+
+public class Forandif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//for loop
+		for(int i=1;i<=10;++i) {
+			
+			//if value of i is between 4 and 9
+			//continue is executed
+			if(i>4&&i<9) {
+				continue;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
