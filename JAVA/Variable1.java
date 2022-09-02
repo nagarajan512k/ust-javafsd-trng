17fb7fadff0713f9bdedff5ccb2a4527a8579a44
nagarajan512k <112387736+nagarajan512k@users.noreply.github.com>
2022-09-02 21:05:26 +0530

    Add files via upload

---
+++ JAVA/Variable1.java
@@ -0,0 +1,27 @@
+package com.ust;
+/**
+ * 
+ * @author ustjavafsdb212
+ *
+ */
+public class Variable1 {
+int age;          //instance variable
+String name;
+float salary;
+int count=10;
+	
+	
+	public static void main(String[] args) {
+		String  str="jayanth";         //local variable
+		// TODO Auto-generated method stub
+//code changes done by Nehila for CR101
+		
+		//end changes nehila
+	}
+
+}
+
+class v{
+	
+
+}
