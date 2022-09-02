ec3ec3797e74351f5a560b531ad157ed5dc160e4
nagarajan512k <112387736+nagarajan512k@users.noreply.github.com>
2022-09-02 20:48:55 +0530

    Add files via upload

---
+++ JAVA/InstanceofDemo2.java
@@ -0,0 +1,22 @@
+package com.ust;
+
+class Fruitt
+{
+	
+}
+class Vehiclee
+{
+
+}
+
+public class InstanceofDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fruitt apple=new Fruitt();
+		Vehiclee car=new Vehiclee();
+		
+		System.out.println("car is a Fruit:"+(apple instanceof Fruitt));
+	}
+
+}
