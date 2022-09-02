efa06854df81d651a82e4c8ba6facb8265ad4e63
nagarajan512k <112387736+nagarajan512k@users.noreply.github.com>
2022-09-02 20:40:45 +0530

    Add files via upload

---
+++ JAVA/Dowhile.java
@@ -0,0 +1,20 @@
+package com.ust;
+
+import java.util.Scanner;
+
+public class Dowhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=1,n=5;
+		
+		//do..while loop from 1 to 5
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<=n);
+		
+	}
+
+}
