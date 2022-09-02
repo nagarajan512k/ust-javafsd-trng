746c1b0e1e413a5d90c3d285d9122cdcba36e0cd
nagarajan512k <112387736+nagarajan512k@users.noreply.github.com>
2022-09-02 20:45:42 +0530

    Add files via upload

---
+++ JAVA/Forusingif.java
@@ -0,0 +1,21 @@
+package com.ust;
+
+public class Forusingif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//forloop
+		for(int i=1;i<=10;++i) {
+			
+			//if the value of i is 5 the loop terminates
+			if(i==5)
+			{
+				break;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
