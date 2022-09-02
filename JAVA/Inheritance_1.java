ff1db8830e6225eaf2d81ff4fd2c0af2309d9713
nagarajan512k <112387736+nagarajan512k@users.noreply.github.com>
2022-09-02 20:47:29 +0530

    Add files via upload

---
+++ JAVA/Inheritance_1.java
@@ -0,0 +1,16 @@
+package com.ust;
+
+ class Inheritance {
+String name="jayanth";
+}
+ 
+ public class Inheritance_1 extends Inheritance{
+	public static void main(String args[]) {
+		Inheritance_1 i=new Inheritance_1();
+		System.out.println(i.name);
+	}
+	 
+ }
+
+
+
