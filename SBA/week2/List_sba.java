76d7146bea7b062a52d293f171133d8a683d7236
nagarajan512k <112387736+nagarajan512k@users.noreply.github.com>
2022-09-12 16:52:22 +0530

    Add files via upload

---
+++ SBA/week2/List_sba.java
@@ -0,0 +1,31 @@
+package com.ust3;
+
+import java.util.ArrayList;
+
+public class List_sba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<String> employee=new ArrayList<>();
+		
+		employee.add("Raja 1");
+		employee.add("Naga 2");
+		employee.add("Sai 3");
+		employee.add("Prince 4");
+		employee.add("Insaf 5");
+		employee.add("Jeyanth 6");
+		employee.add("Arun 7");
+		employee.add("Dani 8");
+		employee.add("rissi 9");
+		employee.add("vicky 10");
+		employee.add("sakthi 11");
+		employee.add("Gowshi 12");
+		employee.add("rithu 13");
+		employee.add("sumi 14");
+		employee.add("suji 15");
+		
+		System.out.println("Array List:Employee Names & Employee ID->"+employee);
+	}
+
+}
