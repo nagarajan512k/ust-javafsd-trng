4de6471d1b15ecb4a6c01c9cc7a800280d327a98
nagarajan512k <112387736+nagarajan512k@users.noreply.github.com>
2022-09-28 10:58:55 +0530

    Add files via upload

---
+++ javascript/sample_2.js
@@ -0,0 +1,9 @@
+var $var12=200;
+function example(){
+    var $var12=300;
+    console.log("Inside example() function="+$var12);
+
+}
+
+console.log("outside example() function="+$var12);
+example();
