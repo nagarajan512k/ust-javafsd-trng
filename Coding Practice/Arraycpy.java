54ead73b789c46815ea3e742a8751e4a07bda191
ustjavafsdb212 <233820@ust-global.com>
2022-10-31 17:38:48 +0530

    change input

--- Coding Practice/Arraycpy.java
+++ Coding Practice/Arraycpy.java
@@ -1,8 +1,16 @@
+/**
+ * The type Arraycpy.
+ */
 class Arraycpy
 {
-public static void main(String args[])
+	/**
+	 * Main.
+	 *
+	 * @param args the args
+	 */
+	public static void main(String args[])
 {
-	int[] a={1,2,3,4,5};
+	int[] a={1,2,3,4,5,6,7};
 	int[] b=new int[a.length];
 
 	for(int i=0;i<a.length;i++)
